b5003f6b88ec3ba7147ac7b55aa0ec8ac78a615a
Sudip Sharma <sudpokhara826@gmail.com>
2025-08-04 20:25:25 +0545

    Ticket Routes : Added the updated status and deleted ticket routes  and User Routes: To get user via email

--- ai-ticket-assistant/routes/ticketRoutes.js
+++ ai-ticket-assistant/routes/ticketRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createTicket, getAllTickets, getTicketById } from '../controllers/ticketController.js';
+import { createTicket, getAllTickets, getTicketById, updateTicketStatus, deleteTicket } from '../controllers/ticketController.js';
 import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 router.get("/",auth,getAllTickets);
 router.get("/:ticketId",auth,getTicketById);
 router.post("/",auth,createTicket);
+router.put("/:ticketId/status",auth,updateTicketStatus);
+router.delete("/:ticketId",auth,deleteTicket);
 
 export default router;
--- ai-ticket-assistant/routes/userRoutes.js
+++ ai-ticket-assistant/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, login, logout, signup, updateUser } from '../controllers/userController.js';
+import { getUsers, getUserByEmail, login, logout, signup, updateUser } from '../controllers/userController.js';
 import { auth } from '../middleware/auth.js';
 
 
@@ -10,5 +10,6 @@ router.post('/login', login);
 router.post('/logout', logout);
 router.put('/update', auth, updateUser);
 router.get('/users', auth, getUsers);
+router.get('/user/:email', auth, getUserByEmail);
 
 export default router;
