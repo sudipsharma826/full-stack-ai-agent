0d77065af693a185ee05efff41c6f5853a40f019
Sudip Sharma <sudpokhara826@gmail.com>
2025-08-05 07:45:40 +0545

    Make the backedn reday for the production : Changed done : Changed the mailtrap to the resend,updated the package.jsona and DockerFile for production ,removed the inngest dev and added inngest env varaible, added the monogb alts

--- ai-ticket-assistant/lib/sendMail.js
+++ ai-ticket-assistant/lib/sendMail.js
@@ -1,26 +1,46 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+
+// export async function sendMail(to, subject, text, html) {
+//     try{
+//     const transporter = nodemailer.createTransport({
+//         host: process.env.MAILTRAP_SMTP_HOST,
+//         port: process.env.MAILTRAP_SMTP_PORT,
+//         secure: true, //always use secure connection
+//         auth: {
+//             user: process.env.MAILTRAP_SMTP_USER,
+//             pass: process.env.MAILTRAP_SMTP_PASS
+//         }
+//     });
+//     await transporter.sendMail({
+//         from: `"AI Ticket Assistant" <${process.env.MAILTRAP_SMTP_USER}>`,
+//         to,
+//         subject,
+//         text,
+//         html
+//     });
+//     console.log('Email sent successfully');
+//    } catch (error) {
+//     console.error('Error sending email:', error.message);
+//     throw error;
+// }
+// }
+//we have test the mail are sent successfully using mailtrap.io
+//now for the real production we will use the resend mail service
+import { Resend } from 'resend';
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendMail(to, subject, text, html) {
-    try{
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAILTRAP_SMTP_HOST,
-        port: process.env.MAILTRAP_SMTP_PORT,
-        secure: true, //always use secure connection
-        auth: {
-            user: process.env.MAILTRAP_SMTP_USER,
-            pass: process.env.MAILTRAP_SMTP_PASS
-        }
-    });
-    await transporter.sendMail({
-        from: `"AI Ticket Assistant" <${process.env.MAILTRAP_SMTP_USER}>`,
-        to,
-        subject,
-        text,
-        html
-    });
-    console.log('Email sent successfully');
-   } catch (error) {
-    console.error('Error sending email:', error.message);
-    throw error;
-}
+    try {
+        await resend.emails.send({
+            from: process.env.MAILTRAP_SMTP_USER,
+            to,
+            subject,
+            text,
+            html
+        });
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error.message);
+        throw error;
+    }
 }
